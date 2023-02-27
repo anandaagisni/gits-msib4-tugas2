78ff262f5b64302c51c4d8a8cf5d39e610635816
ACER <anandaagisni756@gmail.com>
2023-02-27 23:44:29 +0700

    ananda fitria agisni

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<!-- Created By ananda agisni -->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ananda Fitria Agisni</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/typed.js/2.0.11/typed.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>
+
+</head>
+<body>
+    <div class="scroll-up-btn">
+        <i class="fas fa-angle-up"></i>
+    </div>
+    <nav class="navbar">
+        <div class="max-width">
+            <div class="logo"><a href="#">Ananda Fitria<span> Agisni</span></a></div>
+            <ul class="menu">
+                <li><a href="#home" class="menu-btn">Home</a></li>
+                <li><a href="#about" class="menu-btn">About</a></li>
+                <li><a href="#contact" class="menu-btn">Contact</a></li>
+            </ul>
+            <div class="menu-btn">
+                <i class="fas fa-bars"></i>
+            </div>
+        </div>
+    </nav>
+
+    <!-- home section start -->
+    <section class="home" id="home">
+        <div class="max-width">
+            <div class="home-content">
+                <div class="text-1">Hello, my name is</div>
+                <div class="text-2">Ananda Fitria Agisni</div>
+                <div class="text-3">And I'm a <span class="typing"></span></div>
+                <a href="http://wa.me/628788291497/?text=Hai%2CSaya%20mengetahui%20layanan%20dari%20irfan.capcui.com">Whatsapp me</a>
+            </div>
+        </div>
+    </section>
+
+    <!-- about section start -->
+    <section class="about" id="about">
+        <div class="max-width">
+            <h2 class="title">About me</h2>
+            <div class="about-content">
+                <div class="column left">
+                    <img src="images/agisni.jpg" alt="">
+                </div>
+                <div class="column right">
+                    <div class="text">I'm Ananda and I'm a <span class="typing-2"></span></div>
+                    <p>Assalamu’alaikum Wr.Wb, Perkenalkan Saya ananda Mahasiswa Universitas Teknologi Mataram Semester 6 Program Studi Sistem Informasi. saat ini saya sedang mengikuti MSIB Studi Independen Di PT Gits Indonesia selama 6 bulan kedepan,membuat saya terus mempelajari tentang website khususnya dalam dunia full stuck web Developer. Selain itu saya juga menekuni bidang public speaking.<br>
+saya senang karena banyak teman karena itu jangan lupa untuk saling menyapa.
+</p>
+                    <a href="#">Download CV</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- contact section start -->
+    <section class="contact" id="contact">
+        <div class="max-width">
+            <h2 class="title">Contact me</h2>
+            <div class="contact-content">
+                <div class="column left">
+                    <div class="text">Get in Touch</div>
+                    <p>Bila anda ingin mengetahui saya lebih lanjut bisa menghubungi kontak berikut ini. Saya sangat berterimasih bila kita bisa saling suport.</p>
+                    <div class="icons">
+                        <div class="row">
+                            <i class="fas fa-user"></i>
+                            <div class="info">
+                                <div class="head">Name</div>
+                                <div class="sub-title">Ananda Fitria Agisni</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-map-marker-alt"></i>
+                            <div class="info">
+                                <div class="head">Address</div>
+                                <div class="sub-title">Lombok,NTB,Indonesia</div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <i class="fas fa-envelope"></i>
+                            <div class="info">
+                                <div class="head">Email</div>
+                                <div class="sub-title">anandaagisni756@gmail.com</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="column right">
+                    <div class="text">Message me</div>
+                    <form action="http://wa.me/6287882914957/?text=Hai%2CSaya%20ingin%20mengetahui%20produk%20layanan%20dari%20irfan.capcui.com">
+                        <div class="fields">
+                            <div class="field name">
+                                <input type="text" placeholder="Name" required>
+                            </div>
+                            <div class="field email">
+                                <input type="email" placeholder="Email" required>
+                            </div>
+                        </div>
+                        <div class="field">
+                            <input type="text" placeholder="Subject" required>
+                        </div>
+                        <div class="field textarea">
+                            <textarea cols="30" rows="10" placeholder="Message.." required></textarea>
+                        </div>
+                        <div class="button">
+                            <button type="submit">Send message</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- footer section start -->
+    <footer>
+        <span>Created By <a href="https://anandagisni.com">agisni</a> | <span class="far fa-copyright"></span> 2022 All rights reserved.</span>
+    </footer>
+
+    <script src="script.js"></script>
+</body>
+</html>
